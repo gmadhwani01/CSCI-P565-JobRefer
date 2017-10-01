4b0badc0629ec40c64d765175f98ae277cc804cb
Jayendra Khandare <jayendrakhandare@gmail.com>
2017-10-01 12:34:26 -0400

    Added a few functions
    added sending mail option using nodemailer
    added hash function for encrypting password
    added user model for separating schema in ./app/userModel.js 
    
    added a basic http.createServer template

--- mongooseTest.js
+++ mongooseTest.js
@@ -1,68 +1,64 @@
+'use strict';
 
-//  mongoose setup
-var mongoose = require('mongoose');
+var express = require('express'),
+    app = express(),
+    bcrypt = require('bcrypt'),
+    mongoose = require('mongoose'),
+    http = require('http'),
+    fs = require('fs'),
+    nodemailer = require('nodemailer'),
+    smtpTransport = require('nodemailer-smtp-transport');
+
 mongoose.Promise = global.Promise;
 
+//  nodemailer setup
+var transporter = nodemailer.createTransport(smtpTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    secureConnection: true,
+    auth: {
+        user: 'se.researchmate@gmail.com',
+        pass: 'agileteam'
+    }
+}));
+var mailOptions = {
+    from: 'se.researchmate@gmail.com',
+    to: 'jayendrakhandare@gmail.com',
+    subject: 'Sending Email using Node.js[nodemailer]',
+    text: 'Test Successful!'
+};
+
+
+//  hashing function
+var myHasher = function(password) {
+    var hash = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+    return hash;
+};
 
 // Connect to MongoDB on localhost:27017
 mongoose.connect('mongodb://localhost:27017/researchMate', { useMongoClient: true });
 
+//  importing a pre-defined model
+var User = require('./app/userModel');
 
-//  defining schema
-var userSchema = mongoose.Schema({
-    emailID:{
-        type: String,
-        required: true,
-        unique:true
-    },
-    userName: {
-        type: String,
-        required: true,
-        unique:true
-    },
-    firstName:{
-        type: String,
-        required: true
-    },
-    lastName:{
-        type: String,
-        required: true
-    },
-    passWord:{
-        type: String,
-        required: true
-    }
-});
-
-
-// adding a function to schema  --has to be done before using defining model
-userSchema.methods.speak = function () {
-    var greeting = this.name
-        ? "Hello! My name is " + this.name
-        : "Hi! I don't have a name.";
-    console.log(greeting);
-};
-
-
-//  defining model
-var User = mongoose.model('User', userSchema);
-
-
+/*
 //  creating a document
 var addUser = new User({
-    emailID:'h',
-    userName: 'h',
+    emailID:'7',
+    userName: '8',
     firstName: 'j',
     lastName: 'j',
-    passWord:'j'
+    passWord:'jdkflhg'
 });
 
+//  encrypting password
+addUser.passWord = myHasher(addUser.passWord);
 //  adding a document
+
 addUser.save(function (err) {
     if (err) return console.log("User already exists.");
     else console.log("New User Added : " + addUser.userName);
 });
-
 
 //  updating a document     not working properly
 var query = {firstName:'j'};
@@ -71,16 +67,45 @@ User.findOneAndUpdate(query,{firstName:'x'},function(err,upUser){
     else    console.log("Update Successful.");
 });
 
-
 //  viewing a document
 User.findOne({ 'userName': 'k' }, function (err, seeUser) {
     if (seeUser == null) return console.log("Error. User not found.");
     else console.log('%s %s', seeUser.firstName, seeUser.lastName)
 });
 
-
 //  removing a document
 var query = {"userName" : "j"};
 User.remove(query,function(){
     console.log("User Removed Successfully.");
     });
+*/
+
+
+/*
+app.get('/',function(req,res){
+    res.send('app/htmls/index.html');
+});
+
+app.listen(4500);
+console.log("Listening at localhost:4500");
+*/
+
+//to put a html page
+http.createServer(function (req, res) {
+    fs.readFile('./app/htmls/login.html', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+    });
+
+}).listen(8080);
+
+
+//  sending mail using nodemailer
+transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Email sent: ' + info.response);
+    }
+});
