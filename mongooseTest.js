247487e5bd1c71c0b47a2dc410c3fb26948ec0bc
Jayendra Khandare <jayendrakhandare@gmail.com>
2017-10-02 12:25:34 -0400

    changed communication signature
    converted all the back end code to functions so that they will be easy to call from front end

--- mongooseTest.js
+++ mongooseTest.js
@@ -11,107 +11,130 @@ var express = require('express'),
 
 mongoose.Promise = global.Promise;
 
-
 //  hashing function
 var myHasher = function(password) {
     var hash = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
     return hash;
 };
 
-function setupMongo() {
+//  nodemailer setup
+var transporter = nodemailer.createTransport(smtpTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    secureConnection: true,
+    auth: {
+        user: 'se.researchmate@gmail.com',
+        pass: 'agileteam'
+    }
+}));
+
+//  sending mail using nodemailer
+function sendMaill(mailOptions) {
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response +'\nto :' + mailOptions.to);
+        }
+    });
+};
+
 // Connect to MongoDB on localhost:27017
-    mongoose.connect('mongodb://localhost:27017/researchMate', { useMongoClient: true });
+mongoose.connect('mongodb://localhost:27017/researchMate', { useMongoClient: true });
 
 //  importing a pre-defined model
-    var User = require('./app/userModel');
-}
+var User = require('./app/userModel');
 
-setupMongo();
-
-function addingUser() {
+function addingUser(req,res,next) {
 //  creating a document
     var addUser = new User({
-        emailID: 'def',
-        userName: 'ff',
-        firstName: 'j',
-        lastName: 'j',
-        passWord: 'jdkflhg'
+        emailID: 'jayendrakhandare',
+        userName: 'jkhandar',
+        firstName: 'jay',
+        lastName: 'khandare',
+        passWord: 'qwertyuiop[]'
     });
+
+    var mailOptions = {
+        from: 'se.researchmate@gmail.com',
+        to: addUser.emailID+'@gmail.com',
+        subject: 'Verification code',
+        text: 'Your verification code :'+ addUser.verificationNumber
+    };
 
 //  encrypting password
     addUser.passWord = myHasher(addUser.passWord);
 //  adding a document to database
     addUser.save(function (err) {
-        if (err) return console.log("User already exists.");
-        else console.log("New User Added : " + addUser.userName);
-    });
-
-    //  nodemailer setup
-    var transporter = nodemailer.createTransport(smtpTransport({
-        service: 'gmail',
-        host: 'smtp.gmail.com',
-        secureConnection: true,
-        auth: {
-            user: 'se.researchmate@gmail.com',
-            pass: 'agileteam'
+        if (err) {
+            res.send('User already exists.');
+            console.log("User already exists.");
         }
-    }));
-
-    var mailOptions = {
-        from: 'se.researchmate@gmail.com',
-        to: 'jayendrakhandare@gmail.com',
-        subject: 'Sending Email using Node.js[nodemailer]',
-        text: 'Added'
-    };
-
-    sendMaill();
-}
-function updatingUser() {
-//  updating a document
-    var query = {firstName: 'j'};
-    User.findOneAndUpdate(query, {firstName: 'x'}, function (err, upUser) {
-        if (upUser == null) console.log("Failed to Update.");
-        else console.log("Update Successful.");
-    });
-}
-
-function checkingUser() {
-//  viewing a document
-    User.findOne({'userName': 'k'}, function (err, seeUser) {
-        if (seeUser == null) return false;
         else {
-            console.log('%s %s', seeUser.firstName, seeUser.lastName);
-            return true;
+            console.log("New User Added : " + addUser.userName);
+            sendMaill(mailOptions);
+            res.send("New User Added : " + addUser.userName);
         }
     });
-};
+}
+function updatingUser(req,res,next) { //for sprint 2
+//  updating a document
+    var query = {userName: 'jkhandar'};
+    User.findOneAndUpdate(query, {firstName: 'Mr.Jayendra'}, function (err, upUser) {
+        if (upUser == null){
+            res.send('Update Failed.')
+            console.log("Update Failed.");
+        }
+        else {
+            res.send('Update successful.');
+            console.log("Update Successful.");
+        }
+    });
+}
 
-function deletingUser() {
+function checkingUser(req,res,next) {
+//  viewing a document
+    var query = {"userName": "j"};
+    User.findOne(query, function (err, seeUser) {
+        if (seeUser == null){
+            res.send("userName doesn't exist in the database.");
+        }
+        else {
+            res.send('%s %s', seeUser.firstName, seeUser.lastName);
+        }
+    });
+    console.log("Performed a check for userName :" + query.userName);
+}
+
+function deletingUser(req,res,next) {
 //  removing a document
     var query = {"userName": "j"};
     User.remove(query, function () {
         console.log("User Removed Successfully.");
-        return true;
+        res.send("Deleted the user.")
     });
 }
 
-var sayHello = function(req,res,next){
-	res.send("Hello");
-	console.log(req);
-};
+function sayHello(req,res,next){
+    res.send("Hello");
+    console.log("said hello");
+}
 
-//app = connect();
+
+//  accepting various calls to functions from client side
+app.get('/sayHello',sayHello);
+app.get('/addingUser',addingUser);
+app.get('/checkingUser',checkingUser);
+app.get('/deletingUser',deletingUser);
+app.get('/updatingUser',updatingUser);
+
+/*
 app.use(sayHello);
+app.use(addingUser);
+app.use(checkingUser);
+app.use(deletingUser);
+app.use(updatingUser);
+*/
+
 app.listen(23456);
 console.log("Server running at silo.soic.indiana.edu:23456");
-
-//  sending mail using nodemailer
-function sendMaill() {
-transporter.sendMail(mailOptions, function(error, info){
-	if (error) {
-           console.log(error);
-        } else {
-           console.log('Email sent: ' + info.response);
-        }
-});    
-}
